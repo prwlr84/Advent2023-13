dd8a274ce20e0c30de96eed78d9ef122ee078fcb
Antal Bako <antal.bako@tradelink.co>
2024-02-27 22:30:56 +0100

    third

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def parse_input(string):
 
 def calc_mirror_score(grid, vertical=False):
     for i, e in enumerate(grid):
-        while len(grid) - 3 > i > 1:
-            if all([e == grid[i + 1], grid[i - 1] == grid[i + 2]]):
+        while len(grid) - 4 > i > 3:
+            if all([e == grid[i + 1], grid[i - 1] == grid[i + 2], grid[i - 2] == grid[i + 3], grid[i - 3] == grid[i + 4]]):
                 return i + 1 if vertical else (i + 1) * 100
             else:
                 break
@@ -21,12 +21,30 @@ def calc_mirror_score(grid, vertical=False):
     return 0
 
 
+# def mirrors(grids):
+#     count = 0
+#     for grid in grids:
+#         score = calc_mirror_score(grid)
+#         if score == 0:
+#             score = calc_mirror_score([list(row) for row in zip(*grid[::-1])], vertical=True)
+#         count += score
+#
+#     print(count)
+
+
 def mirrors(grids):
     count = 0
     for grid in grids:
+        max = 0
+        current = None
+        for i,line in enumerate(grid):
+            for j in range(len(grid), 1):
+                while j < i - 1:
+
+
         score = calc_mirror_score(grid)
         if score == 0:
-            score = calc_mirror_score([list(row)[::-1] for row in zip(*grid[::-1])], vertical=True)
+            score = calc_mirror_score([list(row) for row in zip(*grid[::-1])], vertical=True)
         count += score
 
     print(count)
